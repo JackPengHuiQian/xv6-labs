d8352ccb130e76e8a3f478b930e1737f72cd9bfe
Your Name <1794709157@qq.com>
2021-10-23 16:15:41 +0800

    thread0-labs 2021/10/21-23:16.15

--- user/uthread.c
+++ user/uthread.c
@@ -2,6 +2,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
 /* Possible states of a thread: */
 #define FREE        0x0
 #define RUNNING     0x1
@@ -10,11 +11,26 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct tcontext{
+  uint64 ra;
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct tcontext tc;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +79,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->tc,(uint64)&next_thread->tc);
   } else
     next_thread = 0;
 }
@@ -77,6 +94,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->tc.ra=(uint64)func;
+  t->tc.sp=(uint64)t->stack+STACK_SIZE;
 }
 
 void 
@@ -161,3 +180,4 @@ main(int argc, char *argv[])
   thread_schedule();
   exit(0);
 }
+
